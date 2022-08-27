26b54fdbc5b6201e2c4118ea98d8835c8bd62dfe
l,m <etedr@gs>
2022-08-27 14:30:01 +0900

    8.27중간

--- ES2015문법/ap06.ES2015문법-cli/es2015test/src/Module1.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/src/Module1.js
@@ -1,7 +1,7 @@
 // Module1.js
 const a = 1;
 const b = 2;
-export { a };
+export { a }; //console.log(a, b, c);
 export const c = 3;
 export default b; // default 는 파일당 1번만 사용 가능
-//a,b,c세개를 내보냈고 내보내는 방식이 각각다르다.
+//a,b,c세개를 내보냈고 내보내는 방식이 각각다르다.  import b, { a, c } from './module1';
---
+++ React/rt21.컴포넌트실습.recipes/recipesprj01/src/Menu.js
@@ -0,0 +1,37 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+  useMemo,
+  useReducer,
+  Fragment,
+  forwardRef,
+  useImperativeHandle
+} from "react";
+import { Recipe } from "./Menu";
+//import Recipe from "./Recipe"; //export default인경우
+
+
+
+//.MENU
+function Menu({ title, recipes }) {
+  // 컴포넌트의 상태값 설정
+  // 상태값이 기본타입인 경우
+  // JSX로 화면 만들기
+  return (
+    <article>
+      <header>
+        <h1>{title}</h1>
+      </header>
+      <div className="recipes">
+        {recipes.map((recipe, i) => (
+          <Recipe key={i} {...recipe} />
+        ))}
+      </div>
+    </article>
+  );
+}
+export { Recipe };
