9e53d094d585ed1511d6ffdf6c8bb53f725ac569
firsthandcraft <opaldust19@gmail.com>
2022-09-03 17:41:26 +0900

    화면쪼개긱starprj01

---
+++ React/rt22.컴포넌트실습.StarRating/starprj01/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import RatingContaier from "./RatingContaier";
+function App() {
+  return (
+    <div>
+      <h2>App</h2>
+      <RatingContaier />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/rt22.컴포넌트실습.StarRating/starprj01/src/Star.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Star({ selected }) {
+  // JSX로 화면 만들기
+  const aaa = selected ? "star selected" : "star";
+  // return <div className={aaa}>&nbsp;</div>;
+  return <div className={selected ? "star selected" : "star"}>&nbsp;</div>;
+}
+
+Star.propTypes = {
+  // props의 프로퍼티 타입 설정. https://ko.reactjs.org/docs/typechecking-with-proptypes.html
+  // 인자명: PropTypes.func.isRequired,
+  selected: PropTypes.bool
+};
+Star.defaultProps = {
+  // props의 디폴트 값 설정. https://ko.reactjs.org/docs/typechecking-with-proptypes.html
+  // 인자명: () => {},
+  selected: false
+};
+
+export default React.memo(Star); // React.memo()는 props 미변경시 컴포넌트 리렌더링 방지 설정
