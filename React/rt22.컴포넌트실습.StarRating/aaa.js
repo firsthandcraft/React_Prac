31ad4953b005ee52a9934c49ab592ede0d4d4e3e
parksoyoung <opaldust19@gmail.com>
2024-01-30 15:29:23 +0900

    제작중

--- React/rt22.컴포넌트실습.StarRating/aaa.js
+++
@@ -1,8 +0,0 @@
-const totalStart = 5;
-
-const arr = [...Array(totalStars).keys()].map((val) => val + 1);
-debugger;
-
-console.log(arr);
-
-//f5
---
+++ my_react_project/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 id="title">fff</h1>
+        <button class="hand">가</button>
+        <button class="hand">바</button>
+        <button class="hand">보</button>
+      </header>
+      <Fragment>
+        <h2>sdfsdf</h2>
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
