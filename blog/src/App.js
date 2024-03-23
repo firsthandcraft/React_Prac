a38fde88d1644051386231e9c856d9ffcd272ac2
handcraft <opaldust19@gmail.com>
2024-03-23 22:52:05 +0900

    ff

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   let post ='맞깁';
+  //state 
+  let [글제목,b]=useState('넘자코드추천') ; //Destructurring 문법
   return (
     <div className="App">
       <button class="py-2 px-4 rounded-lg shadow-md text-white bg-blue-500">
         Click me
       </button>
       <h4>{post}</h4>
+      <div>{글제목}</div>
+      <div>{b}</div>
     </div>
   );
 }
