abd6338e7a071221c13d0f8f4f0db2b3db48ffdb
handcraft <opaldust19@gmail.com>
2024-04-10 15:19:47 +0900

    img

--- my_react_project/src/component/Dice.js
+++ my_react_project/src/component/Dice.js
@@ -12,7 +12,7 @@ import diceRed05 from '../assets/dice-red-5.svg';
 import diceRed06 from '../assets/dice-red-6.svg';
 /** props라는 파라미터를 하나 만들기
 { color: "blue" } */
-const DICE_MAGES = {
+const DICE_IMAGES = {
   blue: [diceBlue01,diceBlue02,diceBlue03,diceBlue04,diceBlue05,diceBlue06],
   red:[diceRed01,diceRed02,diceRed03,diceRed04,diceRed05,diceRed06],
 }
@@ -23,8 +23,8 @@ const DICE_MAGES = {
   const diceImg = props.color === 'red' ? diceRed01 :diceBlue01;
   return <img src={src} alt={alt} />;
 }*/
-function Dice({color,num}) {//function Dice({color='blue',num=1}) {
-  const src = DICE_MAGES[color][num-1];
+function Dice({ color = 'blue', num = 1 }) {
+  const src = DICE_IMAGES[color][num - 1];
   const alt = `${color} ${num}`;
   return <img src={src} alt={alt} />;
 }
